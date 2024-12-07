8ecdda9e5e2fb011c7f5b86f56d966b0914bb50f
Jean-LouisB <fkopf@orange.fr>
2024-12-07 09:30:40 +0100

    cleane always

--- getexif/process.py
+++ getexif/process.py
@@ -1,8 +1,10 @@
 import os
 from datetime import datetime
-from writingFile import fichierJson, writeLog
-from autoId import autoId
-from exifData import monImage
+import ntpath
+import math
+from pathlib import Path
+from exif import Image
+
 
 
 class Process:
@@ -46,4 +48,49 @@ class Process:
         except Exception as erreur:
             print("Erreur ici : ")
             print(erreur)
+
+
+class autoId():
+    def __init__(self):
+        self.id = 0
+    def nextId(self):
+        id= self.id
+        self.id+=1
+        return id
+
+
+class monImage():
+    def __init__(self, path, nextId):
+        self.nextId = nextId
+        self.path = path
+
+    def extractInfo(self):
+        with open(self.path, 'rb') as src:
+            img = Image(src)
+            try:
+                x = img.get('exposure_time')
+                if x < 1:
+                    x = 1 / x
+                    app = '1/{}'.format(math.floor(x))
+                else:
+                    app = math.floor(x)
+                exifDict = {}
+                exifDict["id"] = self.nextId
+                exifDict["fichier"] = ntpath.basename(self.path)
+                exifDict["nom"] = Path(self.path).stem
+                exifDict["focale"] = "{} mm".format(img.get('focal_length'))
+                exifDict["Vitesse"] = "{} sec.".format(app)
+                exifDict["ouverture"] = "F/{}".format(img.get('f_number'))
+                exifDict["iso"] = "{} iso".format(img.get('photographic_sensitivity'))
+                exifDict["comment"] = ""
+            except:
+                with open(self.path, 'rb') as src1:
+                    img = Image(src1)
+                    exifDict = {}
+                    exifDict["id"] = self.nextId
+                    exifDict["fichier"] = ntpath.basename(self.path)
+                    exifDict["nom"] = Path(self.path).stem
+                    exifDict["comment"] = ""
+            finally:
+                return exifDict
 
