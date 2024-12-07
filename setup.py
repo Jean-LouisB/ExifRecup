34316c3dcd2bebdfc3b596d36e5309363ac7771a
Jean-LouisB <fkopf@orange.fr>
2024-12-07 11:40:30 +0100

    prepare to pip

--- setup.py
+++ setup.py
@@ -5,6 +5,21 @@ setup(
     version="2.0.0",
     packages=find_packages(),
     install_requires=["exif"],
+    description="Une bibliothèque pour traiter des images avec EXIF",  # Courte description
+    long_description=open("README.md", "r").read(),  # Description détaillée
+    long_description_content_type="text/markdown",  # Type de contenu (Markdown)
     author="Fabrice Kopf",
-    description="Pour exporter les exifs d'un dossier de photos",
+    author_email="contact@fabricekopf.fr",
+    url="https://github.com/Jean-LouisB/ExifRecup.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",  # Version minimale de Python
+    install_requires=[
+        "exif",  # Dépendances (par exemple : exif)
+        "pathlib",
+    ],
 )
+
